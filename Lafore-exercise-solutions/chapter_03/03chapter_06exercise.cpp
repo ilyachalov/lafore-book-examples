c97f37b50b6adc1b17478c0317723eb76fb9fbb0
ilyachalov <48008056+ilyachalov@users.noreply.github.com>
2019-03-24 10:10:10 +0300

    Add files via upload

---
+++ Lafore-exercise-solutions/chapter_03/03chapter_06exercise.cpp
@@ -0,0 +1,32 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 03chapter_06exercise.cpp
+// Подсчет факториала числа с помощью цикла for.
+// Вставить программу вычисления факториала в цикл, условие выхода — ввод пользователем 0.
+
+#include <io.h> // для функции _setmode
+#include <fcntl.h> // для константы _O_U16TEXT
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// переключение стандартного потока вывода в формат Юникода
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	// переключение стандартного потока ввода в формат Юникода
+	_setmode(_fileno(stdin), _O_U16TEXT);
+
+	unsigned int numb;
+	unsigned long fact; // тип long для результата
+	
+	do {
+		fact = 1;
+		wcout << L"Введите целое число: ";
+		wcin >> numb; // ввод числа
+		for (int j = numb; j > 0; j--) // умножение fact на
+			fact *= j;                 // numb, numb-1, ..., 2, 1
+		wcout << L"Факториал числа равен " << fact << endl;
+	} while (numb != 0);
+
+	return 0;
+}
---
+++ Lafore-exercise-solutions/chapter_03/03chapter_12exercise.cpp
@@ -0,0 +1,49 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 03chapter_12exercise.cpp
+// Программа-калькулятор, выполняющая четыре арифметических действия над
+// обыкновенными дробями (вида a/b)
+
+#include <io.h> // для функции _setmode
+#include <fcntl.h> // для константы _O_U16TEXT
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// переключение стандартного потока вывода в формат Юникода
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	// переключение стандартного потока ввода в формат Юникода
+	_setmode(_fileno(stdin), _O_U16TEXT);
+
+	int a, b, c, d; // числители и знаменатели обыкновенных дробей
+	wchar_t slash;  // знак дроби
+	wchar_t oper;   // знак операции
+	wchar_t ch;
+	
+	do {
+		wcout << L"Введите первую дробь, знак операции и вторую дробь: ";
+		wcin >> a >> slash >> b >> oper >> c >> slash >> d;
+
+		switch (oper)
+		{
+		case '+':
+			wcout << L"Результат операции: " << a * d + b * c << L"/" << b * d << endl;
+			break;
+		case '-':
+			wcout << L"Результат операции: " << a * d - b * c << L"/" << b * d << endl;
+			break;
+		case '*':
+			wcout << L"Результат операции: " << a * c << L"/" << b * d << endl;
+			break;
+		case '/':
+			wcout << L"Результат операции: " << a * d << L"/" << b * c << endl;
+			break;
+		default: wcout << L"Введен неверный знак операции! Попробуйте еще." << endl;
+		}
+
+		wcout << L"Выполнить еще одну операцию (д/н) ? "; wcin >> ch;
+	} while (ch != L'н');
+
+	return 0;
+}
