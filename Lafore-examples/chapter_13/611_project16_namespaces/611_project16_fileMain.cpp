8b1eda60701be0069cf468b695b79caabe9ae310
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-03-20 09:41:56 +0300

    Add files via upload

---
+++ Lafore-examples/chapter_13/611_project16_namespaces/611_project16_fileMain.cpp
@@ -0,0 +1,14 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 611_project16_fileMain.cpp
+
+#include "611_project16_fileA.h"
+#include "611_project16_fileB.h"
+
+using namespace alpha;
+
+int main()
+{
+	funcA();
+	funcB();
+}
