9b83607a7219f797255c3b474eb2da49bc2a5fef
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-03-20 09:31:03 +0300

    Add files via upload

---
+++ Lafore-examples/chapter_13/607_project13_include_guards/607_project13_fileA.cpp
@@ -0,0 +1,10 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 607_project13_fileA.cpp
+
+#include "607_project13_fileH.h"
+
+int globalVar; // объявление и определение
+
+int someClass::memFunc(int n1, int n2) // определение метода
+	{ return n1 + n2; }
