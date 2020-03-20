0b3c3ff2ea3d984bed23d4dbe60bf059feacbaf6
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-03-20 09:20:13 +0300

    Add files via upload

---
+++ Lafore-examples/chapter_13/606_project10_headers/606_project10_fileB.cpp
@@ -0,0 +1,11 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 606_project10_fileB.cpp
+
+#include "606_project10_fileH.h"
+
+int main()
+{
+	gloVar = 5;                    // работа с переменной
+	int gloVarB = gloFunc(gloVar); // работа с функцией
+}
