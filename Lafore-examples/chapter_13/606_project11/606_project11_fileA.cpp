934ab3d9eeeb6575d450edf98fd35b0e2a3d168a
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-03-20 09:22:38 +0300

    Add files via upload

---
+++ Lafore-examples/chapter_13/606_project11/606_project11_fileA.cpp
@@ -0,0 +1,11 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 606_project11_fileA.cpp
+
+#include "606_project11_fileH.h"
+
+int main()
+{
+	someClass obj1;                // создаём объект
+	int var1 = obj1.memFunc(2, 3); // работаем с объектом
+}
---
+++ Lafore-examples/chapter_13/606_project11/606_project11_fileB.cpp
@@ -0,0 +1,11 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// 606_project11_fileB.cpp
+
+#include "606_project11_fileH.h"
+
+int func()
+{
+	someClass obj2;                // создаём объект
+	int var2 = obj2.memFunc(4, 5); // работаем с объектом
+}
