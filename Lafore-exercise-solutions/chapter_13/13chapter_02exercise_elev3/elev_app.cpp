76917016b2ef151d08b72425309198fa2be536f6
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-03-26 17:59:37 +0300

    Add files via upload

---
+++ Lafore-exercise-solutions/chapter_13/13chapter_02exercise_elev3/elev_app.cpp
@@ -0,0 +1,29 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// elev_app.cpp
+// клиентский файл
+
+#include "elev.h" // описатели классов
+
+#include <io.h>       // для функции _setmode
+#include <fcntl.h>    // для константы _O_U16TEXT
+
+int main()
+{
+	// переключение стандартного потока вывода в формат Юникода
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	// переключение стандартного потока ввода в формат Юникода
+	_setmode(_fileno(stdin), _O_U16TEXT);
+
+	building theBuilding;
+
+	while (true)
+	{
+		theBuilding.master_tick();       // запустить главный тик (такт) лифта
+		wait(1000);                      // пауза
+		theBuilding.record_floor_reqs(); // получить запросы с этажей
+		                                 // от пользователя
+	}
+
+	return 0;
+}
