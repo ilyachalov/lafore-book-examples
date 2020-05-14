0d875f1a38082e9559e00d2249b14153824ffcf8
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-05-15 02:37:40 +0300

    Add files via upload

---
+++ Lafore-exercise-solutions/chapter_13/13chapter_04exercise_mylib4_dll_1/mylib.h
@@ -0,0 +1,46 @@
+﻿// исходный текст программы сохранен в кодировке UTF-8 с сигнатурой
+
+// mylib.h
+// интерфейс библиотеки классов (определения классов с объявлениями методов)
+
+#pragma once        // запрет повторных включений
+
+#ifdef ProjectDLL_EXPORTING
+	#define CLASS_DECLSPEC __declspec(dllexport)
+#else
+	#define CLASS_DECLSPEC __declspec(dllimport)
+#endif
+
+#include <string>   // для работы с std::wstring
+
+enum genre_list { Детектив, Фантастика, Классика };
+
+class CLASS_DECLSPEC book // класс, представляющий книгу в магазине
+{
+private:
+	std::wstring title; // название
+	genre_list genre;   // жанр
+	float price;        // цена
+public:
+	// конструктор (определение)
+	book(std::wstring t, genre_list g, float p) : title(t), genre(g), price(p)
+		{ }
+	std::wstring ret_title() const; // вернуть название книги (объявление метода)
+	genre_list ret_genre() const;   // вернуть жанр книги (объявление метода)
+	float ret_price() const;        // вернуть цену книги (объявление метода)
+};
+
+class CLASS_DECLSPEC buyer // класс, представляющий покупателя в магазине
+{
+private:
+	std::wstring name;         // имя покупателя
+	genre_list favorite_genre; // любимый жанр
+	float sum_of_money;        // денежная сумма, которую покупатель готов потратить
+public:
+	// конструктор (определение)
+	buyer(std::wstring n, genre_list fg, float m) : name(n), favorite_genre(fg), sum_of_money(m)
+		{ }
+	std::wstring ret_name() const;  // вернуть имя покупателя (объявление метода)
+	bool examine_genre(book) const; // проверить жанр книги (объявление метода)
+	bool examine_price(book) const; // проверить цену книги (объявление метода)
+};
